a5c51846d2795e6dbebcfc2ad0e1071c60d7e6e4
nicol <nicolas.decat@gmail.com>
2026-01-28 22:23:27 +0100

    Initial commit

---
+++ apps/backend/orchestrator-service/src/main/java/com/nd2k/orchestrator_service/OrchestratorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nd2k.orchestrator_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestratorServiceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(OrchestratorServiceApplication.class, args);
+	}
+
+}
---
+++ apps/backend/orchestrator-service/src/main/java/com/nd2k/orchestrator_service/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.nd2k.orchestrator_service.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return new ResponseEntity<>("TEST OK", HttpStatus.OK);
+    }
+}
